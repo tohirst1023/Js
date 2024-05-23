873fc43d397ebbbfeeef6897ec2f5fdb9c2973f5
tohirst1023 <159892479+tohirst1023@users.noreply.github.com>
2024-05-23 14:04:07 +0500

    dsfsdfsf

---
+++ index.js
@@ -0,0 +1,12 @@
+// let birinchison = Number(prompt("birinchi soni kiriting"));
+// let ikkinchison =Number( prompt("Ikkinchi soni kiriting"));
+
+// alert(birinchison + ikkinchison);
+// alert(birinchison - ikkinchison);
+// alert(birinchison / ikkinchison);
+
+prompt ("Ismingizni kiriting");
+prompt ("Familyangizni kiriting");
+prompt ("Qayerda turdfdfasiz?");
+prompt ("Yoshingizni kiriting");
+
